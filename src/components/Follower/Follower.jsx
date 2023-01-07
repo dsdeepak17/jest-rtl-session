11221eb6dc367a75660511d336f981815b6edbc7
dsdeepaksingh <DSDEEPAK1792@GMAIL.COM>
2023-01-08 00:20:02 +0530

    adds followers component

---
+++ src/components/Follower/Follower.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Follower({ follower }) {
+  const { firstName, lastName, phone, age, image } = follower;
+
+
+
+  return (
+    <div className='follower'>
+      <img src={image} alt="user-img" className='follower-image' />
+      <div className="follower-detail">
+        <span>{`${firstName} ${lastName}`}</span>
+        <span>{`${age} years ${ageEmoji(age)}`}</span>
+        <span>{`☎️: ${phone}`}</span>
+      </div>
+
+    </div>
+  )
+}
+
+export default Follower
+
+const ageEmoji = (age) => {
+  if (age < 10) return '👶'
+  if (age >= 10 && age < 25) return '👦'
+  if (age >= 25 && age < 60) return '👨'
+  return '👴'
+}
--- src/pages/FollowersPage.jsx
+++ src/pages/FollowersPage.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Follower from '../components/Follower/Follower';
+
+const USER_API = 'https://dummyjson.com/users';
+const NUMBER_OF_USERS = 8;
 
 function FollowersPage() {
+  const [followers, setFollowers] = useState([])
+
+  useEffect(() => {
+    console.log({ followers });
+  }, [followers])
+
+  const fetchFollwers = async () => {
+    const data = await fetch(`${USER_API}?limit=${NUMBER_OF_USERS}`);
+    const users = await data.json();
+    setFollowers(users.users)
+  }
+
+  useEffect(() => {
+    fetchFollwers();
+  }, [])
+
   return (
-    <div>FollowersPage</div>
+    <div className='followers-page'>
+      {followers.length === 0 ? 'Loading...' :
+        <div className='follower-list'>
+          {
+            followers?.map(follower => {
+              return <Follower follower={follower} key={follower.id} />
+            })
+          }
+        </div>
+      }
+    </div>
   )
 }
 
