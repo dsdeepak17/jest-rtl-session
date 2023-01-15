e9348ab625f022b0f5bbde21e0dc3c6133f8b523
dsdeepaksingh <DSDEEPAK1792@GMAIL.COM>
2023-01-16 00:23:44 +0530

    install redux and basic setup

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
