78e36ed58504c91beabc36c46260fc9d36988227
Deepak Singh <deepak_singh@softprodigy.com>
2023-01-16 22:02:16 +0530

    adds follower reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
+// import { setupListeners } from '@reduxjs/toolkit/query'
 import todoReducer from './TodoSlice';
+import { followerApi } from './followerApi'
 
 export const store = configureStore({
   reducer: {
-    todos: todoReducer
+    todos: todoReducer,
+    // Add the generated reducer as a specific top-level slice
+    [followerApi.reducerPath]: followerApi.reducer,
   },
+
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(followerApi.middleware),
 })
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+// setupListeners(store.dispatch)
