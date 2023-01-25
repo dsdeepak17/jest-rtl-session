c1e82db927f4e5909c3772e913b29ae89c20dcd4
Deepak Singh <dsdeepak1792@gmail.com>
2023-01-25 18:24:49 +0530

    adds loading facade when data being fetched.

--- src/pages/FollowersPage.jsx
+++ src/pages/FollowersPage.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import Follower from '../components/Follower/Follower';
+import GlimmerLine from '../components/GlimmerLine/GlimmerLine';
 // import { fetchFollowers } from '../api';
 import { useGetFollowersQuery } from '../redux/followerApi';
 
 function FollowersPage() {
   const { data: followers, error, isLoading } = useGetFollowersQuery(10)
 
-if(error){
-  return <>Error has occured</>
-}
+  if (error) {
+    return <>Error has occured</>
+  }
 
   return (
     <div className='followers-page' data-testid='followers-page'>
-      {isLoading ? <span className='centered-item'>Loading...</span> :
+      {isLoading ?
+        <>
+          <GlimmerLine style={{ height: "5.5rem", borderRadius: "var(--border-radius)" }} />
+          <GlimmerLine style={{ height: "5.5rem", borderRadius: "var(--border-radius)" }} />
+          <GlimmerLine style={{ height: "5.5rem", borderRadius: "var(--border-radius)" }} />
+        </>
+        :
         <div className='follower-list' data-testid='follower-list'>
           {
             followers?.map(follower => {
